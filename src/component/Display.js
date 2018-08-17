ed34e714b3c7de18b9d4b25d5c279854651c479d
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-08-17 16:04:27 -0400

    add thousands separator

--- src/component/Display.js
+++ src/component/Display.js
@@ -3,12 +3,23 @@ import PropTypes from "prop-types";
 
 import "./Display.css";
 
+function thousandsSeparators(numberStr) {
+  var x = numberStr.split('.');
+  var x1 = x[0];
+  var x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+      x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return x1 + x2;
+}
+
 class Display extends React.Component {
   render() {
     return (
       <div className="component-display">
         <p>{this.props.message}</p>
-        <div>{this.props.value}</div>
+        <div>{thousandsSeparators(this.props.value)}</div>
       </div>
     );
   }
