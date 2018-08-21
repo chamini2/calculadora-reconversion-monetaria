2f009fd75d7e636cd5a5a34cafc350fdb61ae3a9
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-08-21 13:01:04 -0400

    add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./index.css";
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-69937814-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(<App />, document.getElementById("root"));
